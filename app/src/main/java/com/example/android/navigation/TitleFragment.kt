21cc4210a9b70d7df428716178710f75120d8d7e
Abdo Tareq <abdotarekfathy@gmail.com>
2020-03-28 16:50:25 +0200

    step 02 sol navigation with view binding

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -13,8 +15,10 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
 class TitleFragment : Fragment() {
 
     private var _binding: FragmentTitleBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
+    // Access views from binding not _binding
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -22,12 +26,26 @@ class TitleFragment : Fragment() {
     ): View? {
         _binding = FragmentTitleBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.playButton.setOnClickListener {
+            Toast.makeText(context, "good", Toast.LENGTH_SHORT).show()
+
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+
+        }
+
+
+
         return view
     }
 
+
+    // to prevent memory leaks
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
 
     }
+
+
 }
